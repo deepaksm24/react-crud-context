89a4b881c6d8de793dbbee83b96f7b14a2557f1e
deepaksm24 <dpkmdu@gmail.com>
2023-03-24 09:53:45 +0530

    2n commit

--- src/ADMIN.js
+++ src/ADMIN.js
@@ -17,10 +17,10 @@ export function Admin() {
             <Container className='jusify-content-centre mt-2'>
 
                 <h1>Welcome</h1>
-                <Image className='jusify-content-centre mt-6 p-2'
+                <Image className='img-fluid shadow-4 jusify-content-centre mt-6 p-2'
         src=
 "https://t4.ftcdn.net/jpg/02/89/52/37/360_F_289523749_Asn8QNQFMvWXvYdOzobJpHAobslhUqhU.jpg"
-        roundedCircle
+        roundedCircle responsive
       />
             </Container>
 
